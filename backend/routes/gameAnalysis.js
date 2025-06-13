68f457560e834e57131e89f64e1c380344d0871f
Vaghela Parthivsinh Dilipsinh <128499630+IAmParthivsinh-15@users.noreply.github.com>
2025-06-14 01:30:35 +0530

    path error solved

--- backend/routes/gameAnalysis.js
+++ backend/routes/gameAnalysis.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoutes } from "../middlewares/auth.js";
-import gameAnalysisController from "../controller/gameAnalysisController";
+import gameAnalysisController from "../controller/gameAnalysisController.js";
 
 const router = express.Router();
 
